761141a77d61053de354b39d538370e102a84aa8
zgq7 <1140661106@qq.com>
2019-01-23 10:44:12 +0800

    实现springBoot配置

---
+++ src/main/java/com/dsos/config/MainConfig.java
@@ -0,0 +1,7 @@
+package com.dsos.config;
+
+/**
+ * Created by Administrator on 2019/1/23 0023.
+ */
+public class MainConfig {
+}
---
+++ src/main/java/com/dsos/service/MainService.java
@@ -0,0 +1,7 @@
+package com.dsos.service;
+
+/**
+ * Created by Administrator on 2019/1/23 0023.
+ */
+public class MainService {
+}
