156baf26e9c886c989375f20691b7f416d57136a
zgq7 <1140661106@qq.com>
2019-03-18 23:59:52 +0800

    文件上传问题

--- src/main/java/com/dsos/controller/DrugController.java
+++ src/main/java/com/dsos/controller/DrugController.java
@@ -3,17 +3,16 @@ package com.dsos.controller;
 import com.dsos.modle.view.DrugRecord;
 import com.dsos.service.DrugService;
 import com.google.common.collect.ImmutableMap;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
@@ -38,26 +37,28 @@ public class DrugController {
     public @ResponseBody
     Map<Object, Object> drugInList(HttpServletRequest request) {
         Map<Object, Object> requestMap = new HashMap<>();
-        log.info("drugCode :{} , date : {} , chainId :{} ,updown :{}", request.getParameter("drugCode"), request.getParameter("date"),
-                request.getParameter("chainId"), request.getParameter("updown"));
+        String drugCode = request.getParameter("drugCode");
+        String effectDate = request.getParameter("date");
+        String chainId = request.getParameter("chainId");
+        String updown = request.getParameter("updown");
+        log.info("drugCode :{} , date : {} , chainId :{} ,updown :{}", drugCode, effectDate, chainId, updown);
         requestMap.put("page", request.getParameter("page"));
         requestMap.put("limit", request.getParameter("limit"));
-        requestMap.put("drugCode", request.getParameter("drugCode"));
-        requestMap.put("effectDate", request.getParameter("effectDate"));
-        requestMap.put("chainId", request.getParameter("chainId"));
-        requestMap.put("updown", request.getParameter("updown"));
+        requestMap.put("drugCode", drugCode);
+        requestMap.put("effectDate", effectDate);
+        requestMap.put("chainId", chainId);
+        requestMap.put("updown", updown);
 
+        //isBlank 当参数值为null或含空格时返回true
+        if (StringUtils.isBlank(drugCode) && StringUtils.isBlank(effectDate)
+                && StringUtils.isBlank(chainId) && StringUtils.isBlank(updown)) {
+            List<DrugRecord> records = new ArrayList<>();
+            return ImmutableMap.of("data", records, "count", 0, "code", 0, "msg", "");
+        }
         List<DrugRecord> drugRecordList = drugService.getDrugInfoList(requestMap);
         Integer count = drugService.getCountOfCondition(requestMap);
         log.info("====== 正在获取药品数据 -------->第{}页,行数{},总行数{}", request.getParameter("page"), request.getParameter("limit"), count);
         Map<Object, Object> result = new HashMap<>();
-        for (DrugRecord drugRecord : drugRecordList) {
-            if (drugRecord.getIsAllowedTrade().equals("1")) {
-                drugRecord.setIsAllowedTrade("可交易");
-            } else {
-                drugRecord.setIsAllowedTrade("不可交易");
-            }
-        }
         result.put("data", drugRecordList);
         result.put("code", 0);
         result.put("count", count);
--- src/main/resources/js/drug/main.js
+++ src/main/resources/js/drug/main.js
@@ -3,16 +3,16 @@ layui.use(['table', 'form', 'laydate'], function () {
         form = layui.form,
         laydate = layui.laydate;
 
+    let $ = layui.$;
+
     //日期
     laydate.render({
         elem: '#effectDate',
     });
-
     //日期1
     laydate.render({
         elem: '#date1'
     });
-
     //日期2
     laydate.render({
         elem: '#date2'
@@ -58,8 +58,7 @@ layui.use(['table', 'form', 'laydate'], function () {
         ]]
     });
 
-    let $ = layui.$
-        , active = {
+    let activeOfList = {
         reload: function () {
             let drugCode = $('#drugCode');
             let chainNo = $('#chainNo');
@@ -82,15 +81,15 @@ layui.use(['table', 'form', 'laydate'], function () {
 
     $('.demoTable .layui-btn').on('click', function () {
         let type = $(this).data('type');
-        active[type] ? active[type].call(this) : '';
+        activeOfList[type] ? activeOfList[type].call(this) : '';
     });
 
     //监听提交  上架下架 render ---------------------------------->上架下架列表
-    //商品资料列表
     table.render({
-        elem: '#test2'
+        elem: '#updownTable'
         , title: '上架下架'
-        , url: url
+        , url: '../drug/drugInList'
+        , id: 'updownReload'
         , height: 'full-230'
         , page: { //支持传入 laypage 组件的所有参数（某些参数除外，如：jump/elem） - 详见文档
             layout: ['limit', 'count', 'prev', 'page', 'next', 'skip', 'refresh'] //自定义分页布局
@@ -103,7 +102,7 @@ layui.use(['table', 'form', 'laydate'], function () {
         cols: [[
             {field: 'chainId', width: 110, title: '所属连锁', sort: true}
             , {field: 'drugName', width: 240, title: '药品名称'}
-            , {field: 'drugCode', width: 100, title: '药品编码'}
+            , {field: 'drugCode', minWidth: 100, title: '药品编码', sort: true}
             , {field: 'unitPrice', width: 80, title: '连锁单价', sort: true}
             , {field: 'company', width: 200, title: '公司', sort: true}
             , {field: 'purchaseDate', width: 120, title: '进货日期', sort: true}
@@ -113,47 +112,97 @@ layui.use(['table', 'form', 'laydate'], function () {
         ]]
     });
 
-    //监听提交  出库入库 render
-    form.on('submit(demo3)', function (data) {
-        let params = data.field;
-        let drugCode, chainId, updown, effectDate, url;
-        drugCode = params.drugCode;
-        chainId = params.chainId;
-        updown = params.updown;
-        effectDate = params.effectDate;
-        if (updown == '已上架') {
-            updown = 1;
-        } else {
-            updown = 0;
+    let activeOfUpDown = {
+        reload: function () {
+            let drugCode = $('#drugCode');
+            let chainId = $('#chainId');
+            let option = $('#updown option:selected').val();
+            if (option == '已上架') {
+                option = 1;
+            } else {
+                option = 0;
+            }
+            //执行重载 搜索按钮的class要与table.reload一样
+            table.reload('updownReload', {
+                page: {
+                    curr: 1 //重新从第 1 页开始
+                }
+                , where: {
+                    //设置多个参数时用key封装
+                    drugCode: drugCode.val()
+                    , chainId: chainId.val()
+                    , updown: option
+                }
+            })
+            ;
         }
-        url = '../drug/drugInList?drugCode=' + drugCode + '&chainId=' + chainId + '&updown=' + updown + '&efectDate=' + effectDate;
-        //商品资料列表
-        table.render({
-            elem: '#test3'
-            , title: '出库入库'
-            , url: url
-            , height: 'full-700'
-            , cols: [[
-                {field: 'chainId', width: 110, title: '所属连锁', sort: true}
-                , {field: 'drugName', width: 260, title: '药品名称'}
-                , {field: 'drugCode', width: 100, title: '药品编码'}
-                , {field: 'company', width: 200, title: '公司', sort: true}
-                , {field: 'effectDate', width: 160, title: '保质日期', sort: true}
-                , {field: 'produceDate', width: 160, title: '生产日期', sort: true}
-                , {field: 'isAllowedTrade', width: 80, title: '状态', sort: true, style: 'background-color: #5FB878;'}
-                , {fixed: 'right', title: '操作', toolbar: '#barDemo', width: 120}
-            ]]
-        });
-        return false;
+    };
+
+    $('.updownTable .layui-btn').on('click', function () {
+        let type = $(this).data('type');
+        activeOfUpDown[type] ? activeOfUpDown[type].call(this) : '';
     });
 
+
+    //监听提交  出库入库 render ---------------------------------->出库入库列表
+    table.render({
+        elem: '#InOutTable'
+        , title: '出库入库'
+        , url: '../drug/drugInList'
+        , id: 'InOutTable'
+        //, height: 'full-600'
+        , cols: [[
+            {field: 'chainId', width: 110, title: '所属连锁', sort: true}
+            , {field: 'drugName', width: 260, title: '药品名称'}
+            , {field: 'drugCode', minWidth: 100, title: '药品编码'}
+            , {field: 'company', width: 200, title: '公司', sort: true}
+            , {field: 'effectDate', width: 160, title: '保质日期', sort: true}
+            , {field: 'produceDate', width: 160, title: '生产日期', sort: true}
+            , {field: 'isAllowedTrade', width: 80, title: '状态', sort: true, style: 'background-color: #5FB878;'}
+            , {fixed: 'right', title: '操作', toolbar: '#barDemo', width: 120}
+        ]]
+    });
+
+    let activeOfInOut = {
+        reload: function () {
+            let drugCode = $('#drugCode');
+            let chainId = $('#chainId');
+            let effectDate = $('#effectDate');
+            let option = $('#updown option:selected').val();
+            if (option == '已上架') {
+                option = 1;
+            } else {
+                option = 0;
+            }
+            //执行重载 搜索按钮的class要与table.reload一样
+            table.reload('InOutTable', {
+                page: {
+                    curr: 1 //重新从第 1 页开始
+                }
+                , where: {
+                    //设置多个参数时用key封装
+                    drugCode: drugCode.val()
+                    , effectDate: effectDate.val()
+                    , chainId: chainId.val()
+                    , updown: option
+                }
+            })
+            ;
+        }
+    };
+
+    $('.InOutTable .layui-btn').on('click', function () {
+        let type = $(this).data('type');
+        activeOfInOut[type] ? activeOfInOut[type].call(this) : '';
+    });
+
+    //-------------------------------------------------------------------按钮监听
     //监听行工具事件 -> 上架下架 ->按钮
     table.on('tool(test)', function (obj) {
         let data = obj.data;
         let param = {};
         let url = '../drug/updateDrugDownLoad';
         param.drugCode = data.drugCode;
-        //console.log(data.isAllowedTrade);
         if (obj.event === 'down') {
             if (data.isAllowedTrade == '不可交易') {
                 layer.alert("已下架，请勿重复点击！");
@@ -181,7 +230,7 @@ layui.use(['table', 'form', 'laydate'], function () {
     });
 
     //监听行工具事件 -> 出库入库 ->按钮
-    table.on('tool(test3)', function (obj) {
+    table.on('tool(InOut)', function (obj) {
         let data = obj.data;
         let param = {};
         let url = '../drug/updateDrugDownLoad';
@@ -189,7 +238,7 @@ layui.use(['table', 'form', 'laydate'], function () {
         param.chainId = data.chainId;
         console.log(param);
         if (obj.event === 'delete') {
-            layer.confirm('真的删除吗？', function (index) {
+            layer.confirm('真的删除' + param.drugCode + '吗？', function (index) {
                 drugDownUpLoad(param, url);
                 //只是在页面中移除
                 obj.del();
@@ -213,6 +262,7 @@ layui.use(['table', 'form', 'laydate'], function () {
             , field = obj.field; //得到字段
         layer.msg('[ID: ' + data.id + '] ' + field + ' 字段更改为：' + value);
     });
+
     /**
      * 上架下架、出库入库 ajax
      * @param param 参数 JSON对象，并用JSON.Stringft()封装,post请求
